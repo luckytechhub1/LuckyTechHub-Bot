d01c937ce2d8d2a8ca4f5463cc24fddde9b2101a
Issamuhammad <issamuhammad891@gmail.com>
2025-07-07 03:00:01 +0300

    It's muu

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Lucky Tech Hub Bot',
   author: '‎',
-  botName: "Lucky Tech Hub Bot",
+  botName: "it's muu",
   botOwner: 'Lucky218', // Your name
-  ownerNumber: '256789101112', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '255655750895', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
