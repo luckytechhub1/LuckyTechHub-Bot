1c93374159c66c9e0f5753093d8aaaeafa06fc09
FlorenceFilbert <florencefilbert6@gmail.com>
2025-07-12 17:05:38 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Lucky Tech Hub Bot',
+  packname: 'The Flas⚡Bot',
   author: '‎',
-  botName: "Lucky Tech Hub Bot",
-  botOwner: 'Lucky218', // Your name
-  ownerNumber: '256789101112', //Set your number here without + symbol, just add country code & number without any space
+  botName: "The Flas⚡Bot",
+  botOwner: 'Florence', // Your name
+  ownerNumber: '255677789769', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
